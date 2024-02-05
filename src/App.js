037775f52b89e8df13369d294b391ace83e1ea44
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-02-06 01:25:15 +0530

    web page Structure is Okay...

--- src/App.js
+++ src/App.js
@@ -3,9 +3,45 @@
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <section className="h-100 w-100">
+      <div className="todo-container container h-100 w-100 py-3">
+        <div className="row">
+          <div className="col d-flex flex-column h-100 w-100">
+            <h1 className="title d-flex justify-content-center py-3">My Todos</h1>                                                                                    
+            <div className="row mb-3">
+              <div className="col card">
+                <div className="card-body d-flex flex-row justify-content-between align-items-center">
+                  <div className="item">
+                    <label for="title">Title</label>
+                      <input type="text" id="title"></input>
+                    </div>
+                    <div className="item">
+                      <label for="description">Description</label>
+                      <input type="text" id="description"></input>
+                    </div>
+                    <div className="item">
+                      <button type="button" className="add-btn">Add</button>
+                    </div>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col card">
+                <div className="card-body d-flex flex-row">
+                  <div className="bottom-btn d-flex ">
+                    <button type="button">ToDo</button>
+                    <button type="button">Completed</button>
+                  </div>
+                  <div className="add-section">
+
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
