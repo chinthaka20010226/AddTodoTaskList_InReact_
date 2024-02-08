de58dc5033c98ba0936f47dcdb44b7c32513b527
chinthakaSenarathna <chironprabath@gmail.com>
2024-02-08 17:06:13 +0530

    Starting the Style... -> box-shadow: 1px 1px 1px 1px black;  ????

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
 // import logo from './logo.svg';
-// import './App.css';
+import './App.css';
 
 function App() {
   return (
     <section className="h-100 w-100">
-      <div className="todo-container container h-100 w-100 py-3">
+      <div className="container h-100 w-100 py-3">
         <div className="row">
           <div className="col d-flex flex-column h-100 w-100">
             <h1 className="title d-flex justify-content-center py-3">My Todos</h1>                                                                                    
             <div className="row mb-3">
-              <div className="col card">
+              <div className="todo-container col card">
                 <div className="card-body d-flex flex-row justify-content-between align-items-center">
-                  <div className="item">
+                  <div className="col-4 item d-flex flex-column">
                     <label for="title">Title</label>
-                      <input type="text" id="title"></input>
-                    </div>
-                    <div className="item">
-                      <label for="description">Description</label>
-                      <input type="text" id="description"></input>
-                    </div>
-                    <div className="item">
-                      <button type="button" className="add-btn">Add</button>
-                    </div>
+                    <input type="text" id="title" placeholder="What is new title ???"></input>
+                  </div>
+                  <div className="col-4 item d-flex flex-column">
+                    <label for="description">Description</label>
+                    <input type="text" id="description" placeholder="What is new description ???"></input>
+                  </div>
+                  <div className="col-2 item pt-3">
+                    <button type="button" className="add-btn p-1 px-3">Add</button>
+                  </div>
                 </div>
               </div>
             </div>
             <div className="row">
-              <div className="col card">
+              <div className="todo-container col card">
                 <div className="card-body d-flex flex-row">
                   <div className="bottom-btn d-flex ">
                     <button type="button">ToDo</button>
